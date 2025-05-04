34f70125f755c4d4a75b03bd5a23c9618a9e22bf
Javitha ⸂⸂⸜(രᴗര๑)⸝⸃⸃ <145100063+Javitha27@users.noreply.github.com>
2025-05-04 16:32:23 +0200

    Ajout de alloue_noeud et les rotations

---
+++ src/abrnois.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "abrnois.h"
+
+
+Node * alloue_noeud(char * mot){
+    Node *new_noeud = malloc(sizeof(Node));
+    if (new_noeud == NULL){
+        return NULL;
+    }
+    new_noeud->mot = strdup(mot);
+    if (new_noeud->mot == NULL){
+        free(new_noeud);
+        return new_noeud;
+    }
+    new_noeud->nb_occ = 1;
+    new_noeud->fd = NULL;
+    new_noeud->fg = NULL;
+    return new_noeud;
+}
+
+void rotation_gauche(ABRnois * A){
+    if (*A == NULL || (*A)->fd == NULL){
+        return;
+    }
+    ABRnois pivot = (*A)->fd;
+    (*A)->fd = pivot->fg;
+    pivot->fg = *A;
+    *A = pivot;
+}
+
+void rotation_droite(ABRnois * A){
+    if (*A == NULL || (*A)->fg == NULL){
+        return;
+    }
+    ABRnois pivot = (*A)->fg;
+    (*A)->fg = pivot->fd;
+    pivot->fd = *A;
+    *A = pivot;
+}
+
+void rotation_gauche_droite(ABRnois * A){
+    rotation_gauche(&((*A)->fg)); 
+    rotation_droite(A); 
+}
+
+void rotation_droite_gauche(ABRnois * A){
+    rotation_droite(&((*A)->fd)); 
+    rotation_gauche(A); 
+}
